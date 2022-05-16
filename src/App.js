d0ff3763670332a65c80fcb629876549528748e1
Will Gundy <willgunderson503@gmail.com>
2022-05-16 16:06:46 -0700

    set state and DOM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [query, setQuery] = useState('');
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input onChange={e => setQuery(e.target.value)}/>
+        <button>Search</button>
+      </form>
+      <div>
+        write mapping for Pokemon here
+      </div>
     </div>
   );
 }
