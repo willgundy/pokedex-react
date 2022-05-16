cd07380645f03ae6fd837b49a0c391d2e4e2a934
Will Gundy <willgunderson503@gmail.com>
2022-05-16 16:09:39 -0700

    setup useEffects and handle submit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
-  const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    async function getPokemon() {
+      const response = await getPokemon(filter);
+      
+      setPokemon(response);
+    }
+    getPokemon();
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    getPokemon();
+  }
 
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input onChange={e => setQuery(e.target.value)}/>
+        <input onChange={e => setFilter(e.target.value)}/>
         <button>Search</button>
       </form>
       <div>
